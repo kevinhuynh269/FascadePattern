4358d3d2beeae4c555fff741b4c8bec1ea2def38
kevinhuynh269 <kevinhuynh269@yahoo.com>
2021-05-17 10:06:33 -0500

    started project

---
+++ FascadePatternPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FascadePatternPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
