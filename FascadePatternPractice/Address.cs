1cd802e2acbb0a795862da620e9b1af56444328e
kevinhuynh269 <kevinhuynh269@yahoo.com>
2021-05-17 10:17:08 -0500

    added Address and Inventory Service

---
+++ FascadePatternPractice/Address.cs
@@ -0,0 +1,11 @@
+﻿namespace FascadePatternPractice
+{
+    public class Address
+    {
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ FascadePatternPractice/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FascadePatternPractice
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public Decimal Pricing { get; set; }
+        public int Quantity { get; set; }
+        
+    }
+}
