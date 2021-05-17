399f5606d191ff7b6647097efdaf4b1360693574
kevinhuynh269 <kevinhuynh269@yahoo.com>
2021-05-17 13:03:16 -0500

    added override ToString() to Address

--- FascadePatternPractice/Models/Address.cs
+++ FascadePatternPractice/Models/Address.cs
@@ -8,6 +8,9 @@
         public int ZipCode { get; set; }
         public string City { get; set; }
 
-        
+        public override string ToString()
+        {
+            return $"{nameof(Address1)}: {Address1}, {nameof(Address2)}: {Address2}, {nameof(State)}: {State}, {nameof(ZipCode)}: {ZipCode}, {nameof(City)}: {City}";
+        }
     }
 }
